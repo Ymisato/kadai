00c8e347fff98d4c677b5b9ea008a0a0e3708ccb
Ymisato <12T5087B@shinshu-u.ac.jp>
2015-05-21 12:51:09 +0900

    kadai5

---
+++ kadai5.py
@@ -0,0 +1,24 @@
+def countdown(n):
+    if n == 0:
+        print 'Blastoff'
+    else:
+        print n
+        countdown(n-1)
+
+def print_n(s, n):
+    if n <= 0:
+        return
+    print s
+    print_n(s, n-1)
+    
+def do_n(f,n):
+    if n<=0:
+        return
+    f()
+    do_n(f,n-1)
+    
+def f():
+    print "aaa"
+ 
+print_n('Hello',2)   
+#do_n(f,5)
