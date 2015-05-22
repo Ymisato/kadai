6ea0eef928cd5a5fe3c6611c5fbd8e984b2f3772
Ymisato <12T5087B@shinshu-u.ac.jp>
2015-05-22 10:05:31 +0900

    kadai6

---
+++ kadai6.py
@@ -0,0 +1,33 @@
+
+import math
+
+#6.1
+def compare(x,y):
+	if x > y:
+		return 1
+	elif x < y:
+		return -1
+	else:
+		return 0
+        
+print compare(2,1)
+print compare(1,2)
+print compare(1,1)
+
+#6.2    
+def hypotenuse(x,y):
+    square = x**2 + y**2
+    z = math.sqrt(square)
+    return z
+    
+print hypotenuse(3,4)
+    
+#6.3
+def is_between(x, y, z):
+    if x <= y <=z:
+		return True
+    else:
+        return False
+
+print is_between(2,4,6)
+
